77f216c2aed4d5e2a3a2ba6f1c80a7c387dd9ca1
Carlosnova1 <tu-email@example.com>
2026-05-08 23:53:18 -0400

    Estructura correcta para compilar APK Android

---
+++ game/game.go
@@ -0,0 +1,21 @@
+﻿package game
+
+import "github.com/Carlosnova1/strellas/game"
+
+func main() {
+game.Main()
+}
+
+// Main inicia el juego (para Android)
+func Main() {
+ebiten.SetWindowSize(360, 640)
+ebiten.SetWindowTitle("✨ Strellas: Princesa del Saber ✨")
+ebiten.SetWindowResizable(true)
+ebiten.SetTPS(60)
+
+game := NewGame()
+
+if err := ebiten.RunGame(game); err != nil {
+log.Fatal(err)
+}
+}
---
+++ mobile/mobile.go
@@ -0,0 +1,9 @@
+﻿package main
+
+import (
+"github.com/Carlosnova1/strellas/game"
+)
+
+func init() {
+game.Main()
+}
